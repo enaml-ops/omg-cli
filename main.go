93af3fd853ad23a4a93b16f10c15e7fee4c28035
John Calabrese <jcalabrese@pivotal.io>
2016-05-18 17:07:59 -0400

    [#119677027] adding azure to cli

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/bosh-ops/bosh-install/aws-cli"
+	"github.com/bosh-ops/bosh-install/azure-cli"
 	"github.com/codegangsta/cli"
 )
 
@@ -15,12 +15,10 @@ func main() {
 	app.Version = Version
 	app.Commands = []cli.Command{
 		{
-			Name:  "azure",
-			Usage: "azure [--flags] - deploy a bosh to azure",
-			Action: func(c *cli.Context) {
-				fmt.Println("not yet implemented")
-				os.Exit(1)
-			},
+			Name:   "azure",
+			Usage:  "azure [--flags] - deploy a bosh to azure",
+			Action: azurecli.GetAction(BoshInitDeploy),
+			Flags:  azurecli.GetFlags(),
 		},
 		{
 			Name:   "aws",
