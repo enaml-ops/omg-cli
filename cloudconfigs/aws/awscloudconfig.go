d32d98d314686b8039b41fb0c09eee592b491b8c
John Calabrese <jcalabrese@pivotal.io>
2016-05-19 16:46:08 -0400

    [#119868047] stubs of aws cloud config plugin

---
+++ cloudconfigs/aws/awscloudconfig.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/bosh-ops/bosh-install/plugin/cloudconfig"
+	"github.com/codegangsta/cli"
+	"github.com/xchapter7x/enaml"
+)
+
+func main() {
+	cloudconfig.Run(new(AWSCloudConfig))
+}
+
+type AWSCloudConfig struct{}
+
+func (s *AWSCloudConfig) GetFlags() (flags []cli.Flag) {
+	return
+}
+
+func (s *AWSCloudConfig) GetMeta() cloudconfig.Meta {
+	return cloudconfig.Meta{
+		Name: "aws",
+	}
+}
+
+func (s *AWSCloudConfig) GetCloudConfig(c *cli.Context) enaml.CloudConfigManifest {
+	return enaml.CloudConfigManifest{}
+}
