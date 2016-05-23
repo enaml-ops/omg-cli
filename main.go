5b8ffb44fdde8f38181217844a14232eb0660240
John Calabrese <jcalabrese@pivotal.io>
2016-05-23 14:26:44 -0400

    [#119868047] first pass of cloudconfig upload to bosh

--- main.go
+++ main.go
@@ -6,14 +6,17 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"path"
 	"strings"
 
 	"github.com/codegangsta/cli"
+	"github.com/enaml-ops/enaml"
 	"github.com/enaml-ops/omg-cli/aws-cli"
 	"github.com/enaml-ops/omg-cli/azure-cli"
 	"github.com/enaml-ops/omg-cli/plugin/registry"
+	"github.com/enaml-ops/omg-cli/utils/boshapi"
 	"github.com/pivotalservices/gtils/osutils"
 	"github.com/xchapter7x/lo"
 )
@@ -163,6 +166,7 @@ func registerCloudConfig() {
 func getBoshAuthFlags() []cli.Flag {
 	return []cli.Flag{
 		cli.StringFlag{Name: "bosh-url", Value: "https://mybosh.com", Usage: "this is the url or ip of your bosh director"},
+		cli.IntFlag{Name: "bosh-port", Value: 25555, Usage: "this is the port of your bosh director"},
 		cli.StringFlag{Name: "bosh-user", Value: "bosh", Usage: "this is the username for your bosh director"},
 		cli.StringFlag{Name: "bosh-pass", Value: "", Usage: "this is the pasword for your bosh director"},
 		cli.BoolFlag{Name: "print-manifest", Usage: "if you would simply like to output a manifest the set this flag as true."},
@@ -170,13 +174,25 @@ func getBoshAuthFlags() []cli.Flag {
 }
 
 func processManifest(c *cli.Context, manifest []byte) (e error) {
-	yamlString := string(manifest)
 
 	if c.Parent().Bool("print-manifest") {
+		yamlString := string(manifest)
 		fmt.Println(yamlString)
 
 	} else {
-		fmt.Println("TODO: do something with my manifest here", manifest)
+		ccm := enaml.NewCloudConfigManifest(manifest)
+		boshclient := boshapi.NewClient(c.Parent().String("bosh-user"), c.Parent().String("bosh-pass"), c.Parent().String("bosh-url"), c.Parent().Int("bosh-port"))
+		if req, err := boshclient.NewCloudConfigRequest(*ccm); err == nil {
+			httpClient := new(http.Client)
+
+			if res, err := httpClient.Do(req); err != nil {
+				lo.G.Error("res: ", res)
+				lo.G.Error("error: ", err)
+				e = err
+			}
+		} else {
+			e = err
+		}
 	}
 	return
 }
