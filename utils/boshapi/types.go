d27888ef9d25de7e7dccfc0a73f154e4bc6a4c29
John Calabrese <jcalabrese@pivotal.io>
2016-05-23 13:51:33 -0400

    [#119868047] beginnings of a boshapi client for uploading manifests

---
+++ utils/boshapi/types.go
@@ -0,0 +1,17 @@
+package boshapi
+
+type Client struct {
+	user string
+	pass string
+	host string
+	port int
+}
+
+func NewClient(user, pass, host string, port int) *Client {
+	return &Client{
+		user: user,
+		pass: pass,
+		host: host,
+		port: port,
+	}
+}
