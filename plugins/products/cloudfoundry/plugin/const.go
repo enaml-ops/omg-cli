49679eaa28b4617eb80ece781dd537d9ac3d062d
John Calabrese <jcalabrese@pivotal.io>
2016-07-05 14:53:57 -0400

    [#123123581] adding all required releases to deployment def

--- plugins/products/cloudfoundry/plugin/const.go
+++ plugins/products/cloudfoundry/plugin/const.go
@@ -16,6 +16,14 @@ const (
 
 	//DiegoReleaseName
 	DiegoReleaseName = "diego"
+
+	CFMysqlReleaseName         = "cf-mysql"
+	CFLinuxReleaseName         = "cflinuxfs2-rootfs"
+	EtcdReleaseName            = "etcd"
+	PushAppsReleaseName        = "push-apps-manager-release"
+	NotificationsReleaseName   = "notifications"
+	NotificationsUIReleaseName = "notifications-ui"
+	CFAutoscalingReleaseName   = "cf-autoscaling"
 )
 
 var (
@@ -25,6 +33,18 @@ var (
 	CFReleaseVersion = "235.5"
 	//StemcellVersion -
 	StemcellVersion = "3232.4"
+	//DiegoReleaseVerion
+	DiegoReleaseVersion = "0.1467.0"
+	//CFMysqlReleaseVersion
+	CFMysqlReleaseVersion = "25.2"
+
+	GardenReleaseVersion          = "0.337.0"
+	CFLinuxReleaseVersion         = "1.3.0"
+	EtcdReleaseVersion            = "48"
+	PushAppsReleaseVersion        = "621"
+	NotificationsReleaseVersion   = "19"
+	NotificationsUIReleaseVersion = "10"
+	CFAutoscalingReleaseVersion   = "28"
 )
 
 var factories []InstanceGrouperFactory
