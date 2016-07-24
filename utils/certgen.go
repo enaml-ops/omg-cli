035691c14e51d29991381fac1e5a7e8ef5897370
Caleb Washburn <cwashburn@pivotal.io>
2016-07-23 19:14:50 -0700

    updated ssl cert generation to use square/certstrap library

--- utils/certgen.go
+++ utils/certgen.go
@@ -1,109 +1,93 @@
 package utils
 
 import (
-	"crypto/ecdsa"
-	"crypto/rand"
-	"crypto/rsa"
 	"crypto/x509"
-	"crypto/x509/pkix"
-	"encoding/pem"
 	"fmt"
-	"log"
-	"math/big"
 	"net"
-	"os"
-	"time"
+
+	"github.com/square/certstrap/pkix"
 )
 
-func pemBlockForKey(priv interface{}) (block *pem.Block, err error) {
-	block = nil
-	switch k := priv.(type) {
-	case *rsa.PrivateKey:
-		block = &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(k)}
-		return
-	case *ecdsa.PrivateKey:
-		var b []byte
-		b, err = x509.MarshalECPrivateKey(k)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Unable to marshal ECDSA private key: %v", err)
+const (
+	orgUnit     = "enaml-ops"
+	caCertYears = 10
+	signYears   = 2
+	org         = "omg"
+	country     = "US"
+)
+
+func GenerateCert(hosts []string) (caCert, cert, key string, err error) {
+	var cakey, crtKey *pkix.Key
+	var cacrt, crt *pkix.Certificate
+	if cakey, cacrt, err = initialize("enaml"); err == nil {
+		if crt, crtKey, err = createCert(cacrt, cakey, hosts); err != nil {
 			return
 		}
-		block = &pem.Block{Type: "EC PRIVATE KEY", Bytes: b}
-	default:
-		block = nil
+		crtBytes, _ := crt.Export()
+		cert = string(crtBytes[:])
+
+		crkKeyBytes, _ := crtKey.ExportPrivate()
+		key = string(crkKeyBytes[:])
+
+		caCrtBytes, _ := cacrt.Export()
+		caCert = string(caCrtBytes[:])
 	}
 
 	return
 }
 
-//GenerateCert - will generate a cert based on hosts and if it is CA
-func GenerateCert(hosts []string) (caCert, cert, key string, err error) {
-	var caKey, certKey *rsa.PrivateKey
-	var caCertBytes, certBytes []byte
-	var serialNumber *big.Int
-	notBefore := time.Now()
-	oneYear := 365 * 24 * time.Hour
-	notAfter := notBefore.Add(oneYear)
-	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
-	if serialNumber, err = rand.Int(rand.Reader, serialNumberLimit); err != nil {
-		log.Fatalf("failed to generate serial number: %s", err)
+func initialize(commonName string) (key *pkix.Key, crt *pkix.Certificate, err error) {
+	if key, err = pkix.CreateRSAKey(2048); err != nil {
+		return
+	}
+	if crt, err = pkix.CreateCertificateAuthority(key, orgUnit, caCertYears, org, "", "", "", commonName); err != nil {
+		return
+	}
+	return
+}
+
+func createCert(cacrt *pkix.Certificate, cakey *pkix.Key, hosts []string) (crt *pkix.Certificate, csrKey *pkix.Key, err error) {
+	var csr *pkix.CertificateSigningRequest
+	var rawCrt *x509.Certificate
+	var name = ""
+	var domains []string
+	var ips []net.IP
+
+	// Validate that crt is allowed to sign certificates.
+	if rawCrt, err = cacrt.GetRawCertificate(); err != nil {
 		return
 	}
 
-	ca := &x509.Certificate{
-		SerialNumber: serialNumber,
-		Subject: pkix.Name{
-			Organization: []string{"enaml-ops"},
-		},
-		NotBefore:             notBefore,
-		NotAfter:              notAfter,
-		SubjectKeyId:          []byte{1, 2, 3, 4, 5},
-		BasicConstraintsValid: true,
-		IsCA:        true,
-		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
-		KeyUsage:    x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
-	}
-
-	if caKey, err = rsa.GenerateKey(rand.Reader, 2048); err != nil {
+	if !rawCrt.IsCA {
+		err = fmt.Errorf("Selected CA certificate is not allowed to sign certificates")
 		return
-	}
-	if certKey, err = rsa.GenerateKey(rand.Reader, 2048); err != nil {
-		return
-	}
-	caCertBytes, err = x509.CreateCertificate(rand.Reader, ca, ca, &caKey.PublicKey, caKey)
-
-	template := x509.Certificate{
-		SerialNumber: serialNumber,
-		Subject: pkix.Name{
-			Organization: []string{"enaml-ops"},
-		},
-		NotBefore: notBefore,
-		NotAfter:  notAfter,
-
-		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
-		BasicConstraintsValid: true,
 	}
 	for _, h := range hosts {
 		if ip := net.ParseIP(h); ip != nil {
-			template.IPAddresses = append(template.IPAddresses, ip)
+			ips = append(ips, ip)
 		} else {
-			template.DNSNames = append(template.DNSNames, h)
+			domains = append(domains, h)
 		}
 	}
 
-	certBytes, err = x509.CreateCertificate(rand.Reader, &template, ca, &certKey.PublicKey, certKey)
-	if err != nil {
-		log.Fatalf("Failed to create certificate: %s", err)
+	switch {
+	case len(domains) != 0:
+		name = domains[0]
+	case len(ips) != 0:
+		name = ips[0].String()
+	default:
+		err = fmt.Errorf("Must provide Common Name or SAN")
 		return
 	}
 
-	caCert = string(pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: caCertBytes}))
-	cert = string(pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certBytes}))
-	var keyBlock *pem.Block
-	if keyBlock, err = pemBlockForKey(certKey); err == nil {
-		keyBytes := pem.EncodeToMemory(keyBlock)
-		key = string(keyBytes)
+	if csrKey, err = pkix.CreateRSAKey(2048); err != nil {
+		return
+	}
+	if csr, err = pkix.CreateCertificateSigningRequest(csrKey, orgUnit, ips, domains, org, country, "", "", name); err != nil {
+		return
+	}
+	if crt, err = pkix.CreateCertificateHost(cacrt, cakey, csr, signYears); err != nil {
+		return
 	}
 	return
 }
--- utils/certgen_test.go
+++ utils/certgen_test.go
@@ -1,6 +1,9 @@
 package utils_test
 
 import (
+	"crypto/x509"
+	"encoding/pem"
+
 	. "github.com/enaml-ops/omg-cli/utils"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -9,22 +12,40 @@ import (
 var _ = Describe("certgen", func() {
 
 	Describe("given GenerateCert", func() {
+		BeforeEach(func() {
+
+		})
 		Context("when called hosts", func() {
 			It("then it should return valid cacert, cert and key", func() {
-				caCert, cert, key, err := GenerateCert([]string{"*.sys.test.com", "*.apps.test.com"})
+				caCert, cert, key, err := GenerateCert([]string{"*.sys.test.com", "*.apps.test.com", "sys.test.com"})
 				Ω(err).Should(BeNil())
 				Ω(len(caCert) > 0).Should(BeTrue())
 				Ω(len(cert) > 0).Should(BeTrue())
 				Ω(len(key) > 0).Should(BeTrue())
-			})
-		})
-		Context("when called empty hosts and ca", func() {
-			It("then it should return valid cacert, cert and key", func() {
-				caCert, cert, key, err := GenerateCert(nil)
-				Ω(err).Should(BeNil())
-				Ω(len(caCert) > 0).Should(BeTrue())
-				Ω(len(cert) > 0).Should(BeTrue())
-				Ω(len(key) > 0).Should(BeTrue())
+
+				roots := x509.NewCertPool()
+				ok := roots.AppendCertsFromPEM([]byte(caCert))
+				if !ok {
+					panic("failed to parse root certificate")
+				}
+
+				block, _ := pem.Decode([]byte(cert))
+				if block == nil {
+					panic("failed to parse certificate PEM")
+				}
+				certificate, theError := x509.ParseCertificate(block.Bytes)
+				if theError != nil {
+					panic("failed to parse certificate: " + theError.Error())
+				}
+
+				opts := x509.VerifyOptions{
+					DNSName: "sys.test.com",
+					Roots:   roots,
+				}
+
+				if _, theError := certificate.Verify(opts); theError != nil {
+					panic("failed to verify certificate: " + theError.Error())
+				}
 			})
 		})
 	})
